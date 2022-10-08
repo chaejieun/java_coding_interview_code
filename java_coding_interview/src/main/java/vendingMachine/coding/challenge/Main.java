31b6060f7ee62539cec980631649936fa61bf5aa
chaejieun <pettojim@naver.com>
2022-10-09 00:50:50 +0900

    FEAT: vendingMachine

---
+++ java_coding_interview/src/main/java/vendingMachine/coding/challenge/Main.java
@@ -0,0 +1,13 @@
+package main.java.vendingMachine.coding.challenge;
+
+import java.util.List;
+import java.util.Set;
+
+import main.java.vendingMachine.coding.challenge.vendingMachine.VendingMachine;
+
+
+public class Main {
+    public static void main(String[] args) {
+        VendingMachine vm = new VendingMachine();
+    }
+}
